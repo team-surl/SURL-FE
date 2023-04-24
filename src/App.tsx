5242763ad551a1c7cf6c9f4c4b8f0faeaa15c452
Starving._.Minsung <kms73033222@gmail.com>
2023-04-24 17:31:01 +0900

    feat : favicon 설정, 라우트 설정

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,18 @@ import React from "react";
 import Main from "./components/main";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Statistics from "./components/statistics";
 
 function App() {
   return (
-    <>
+    <BrowserRouter>
       <ToastContainer />
-      <Main></Main>
-    </>
+      <Routes>
+        <Route path="/" element={<Main />} />
+        <Route path="/statistics" element={<Statistics />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
